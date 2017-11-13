b257114952cd8804886c5afecf1d98ecd6540d61
Ian Martin <ian@imartin.net>
2017-11-13 09:37:21 -0700

    Move common queue interface to JS object  instead of being a stateful class

--- server/common/queues.js
+++ server/common/queues.js
@@ -1,5 +1,4 @@
 import Queue from 'bull';
-import Arena from 'bull-arena';
 import logger from './logger';
 import { core as config } from './conf';
 
@@ -14,13 +13,6 @@ export default {
       logger.debug(`Redis connection info: ${host}:${port} (password: ${password ? 'yes' : 'no'})`);
       this._instance = {
         queues: mapBullQueues(),
-        arenaQueues: mapArenaQueues(),
-        arena(listenPort = config.get('listen')) {
-          return Arena(
-            { queues: mapArenaQueues() },
-            { port: listenPort, disableListen: true },
-          );
-        },
         get ripQ() {
           return this.queues.find(q => q.name === 'disc ripping');
         },
@@ -46,51 +38,3 @@ function mapBullQueues() {
     return newQ;
   });
 }
-
-function mapArenaQueues() {
-  return config.get('queues').map(q => ({
-    name: q.name,
-    port,
-    host,
-    hostId: 'ncodr',
-  }));
-}
-//
-// export default class Queues {
-//   constructor() {
-//     logger.debug(`Redis connection info: ${host}:${port} (password: ${password ? 'yes' : 'no'})`);
-//     this.queues = config.get('queues').map(q => {
-//       const newQ = Queue(q.name, {
-//         redis: {
-//           host,
-//           port,
-//           password,
-//         },
-//       });
-//       logger.info(`Created ${newQ.name} queue (${q.id})`, newQ);
-//       return newQ;
-//     });
-//     this.arenaQueues = this.queues.map(q => ({
-//       name: q.name,
-//       port,
-//       host,
-//       hostId: 'ncodr',
-//     }));
-//   }
-//
-//   arena(port = config.get('listen')) {
-//     return Arena({ queues: this.arenaQueues }, { port, disableListen: true });
-//   }
-//
-//   queues() {
-//     return this.queues;
-//   }
-//
-//   get ripQ() {
-//     return this.queues.find(q => q.name === 'disc ripping');
-//   }
-//
-//   get encodeQ() {
-//     return this.queues.find(q => q.name === 'video encoding');
-//   }
-// }
