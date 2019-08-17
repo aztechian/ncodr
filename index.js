68568fc5a38cd3a83774e495e876a94885dec318
Ian Martin <ian@imartin.net>
2019-08-17 14:10:17 -0600

    don't use babel-register at runtime

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 global.Promise = require('bluebird'); // override Promise to be bluebird's implementation
-require('@babel/register'); // eslint-disable-line import/no-extraneous-dependencies
 require('./server');
